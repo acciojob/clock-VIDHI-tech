cf7bca641dd0e74f20ff88ba082828738668b97a
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-05-04 20:54:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function updateTime() {
+  const now = new Date();
+  const timer = document.getElementById('timer');
+  timer.textContent = now.toLocaleString();
+}
+
+// Update the timer every second
+setInterval(updateTime, 1000);

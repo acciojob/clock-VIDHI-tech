7d9726b1ab5d14e33c45f25c233feac1f996a2bc
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-06-01 14:38:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function updateTimer(params) {
 	const now = new Date();
-	const timer = document.getElementById(timer);
+	const timer = document.getElementById("timer");
 	timer.textcontent = now.toLocaleString();
 }
 setInterval = (updateTimer, 1000);
